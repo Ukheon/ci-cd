ae800bfc92fe6405c48e7d4fcfe9dfcb321d732e
Ukheon <dnrgjs33@naver.com>
2024-04-23 21:00:56 +0900

    test: cicd test

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>테스트가 실패한다면</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                {count > 0 ? (
-                    <p>
-                        <code>The count is now: {count}</code>
-                    </p>
-                ) : null}
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
+	return <>안녕</>;
 }
 
 export default App;
