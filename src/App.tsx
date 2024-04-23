404aec265b067ee50b5d2def30a91376ca47d475
Ukheon <dnrgjs33@naver.com>
2024-04-23 21:02:32 +0900

    test: app 복원

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	return <>안녕</>;
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src={viteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://react.dev' target='_blank'>
+					<img
+						src={reactLogo}
+						className='logo react'
+						alt='React logo'
+					/>
+				</a>
+			</div>
+			<h1>테스트가 실패한다면</h1>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				{count > 0 ? (
+					<p>
+						<code>The count is now: {count}</code>
+					</p>
+				) : null}
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
 }
 
 export default App;
